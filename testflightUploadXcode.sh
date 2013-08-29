91be0719eee67710aa056d75a3d45818a19f7d5a
Carlin <email.me@carlinyuen.com>
2013-08-28 23:54:09 -0400

    updating with more comments

--- testflightUploadXcode.sh
+++ testflightUploadXcode.sh
@@ -4,40 +4,57 @@
 # Taken from Justin Miller (http://developmentseed.org/blog/2011/sep/02/automating-development-uploads-testflight-xcode/)
 # Place in Xcode Scheme's Archive Post-actions
 
+
+############################################
+# Modify these variables with your own
+
+# Upload API token, from Testflight site (https://testflightapp.com/account/#api)
 API_TOKEN="__YOUR_API_TOKEN__"
+
+# Team token, from Testflight site (https://testflightapp.com/dashboard/team/edit/)
 TEAM_TOKEN="__YOUR_TEAM_TOKEN__"
+
+# Signing identity name (can be taken from Xcode), usually looks like:
+#	iPhone Distribution: Carlin Yuen (ABCDEFG)
 SIGNING_IDENTITY="__YOUR_SIGNING_IDENTITY__"
-PROVISIONING_PROFILE="${HOME}/Library/MobileDevice/Provisioning Profiles/__YOUR_PROVISIONING_PROFILE_.mobileprovision"
-#LOG="/tmp/testflight.log"
-GROWL="${HOME}/bin/growlnotify -a Xcode -w"
+
+# Path to provisioning profile, recommend putting in this dir, but can be custom
+PROVISIONING_PROFILE="${HOME}/Library/MobileDevice/Provisioning Profiles/__YOUR_PROVISIONING_PROFILE__.mobileprovision"
+
+# Url to open in browser after upload is complete
+DONE_URL="https://testflightapp.com/dashboard/builds/"
+
+#LOG="/tmp/testflight.log"	# Uncomment to get log file
+
+
+##############################################
+# Generally don't have to modify after here
 
 DATE=$( /bin/date +"%Y-%m-%d" )
 ARCHIVE=$( /bin/ls -t "${HOME}/Library/Developer/Xcode/Archives/${DATE}" | /usr/bin/grep xcarchive | /usr/bin/sed -n 1p )
 DSYM="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/dSYMs/${PRODUCT_NAME}.app.dSYM"
 APP="${HOME}/Library/Developer/Xcode/Archives/${DATE}/${ARCHIVE}/Products/Applications/${PRODUCT_NAME}.app"
 
-#/usr/bin/open -a /Applications/Utilities/Console.app $LOG
-
-#echo -n "Creating .ipa for ${PRODUCT_NAME}... " > $LOG
-echo "Creating .ipa for ${PRODUCT_NAME}" | ${GROWL}
+if [ -n "$LOG" ]; then
+	/usr/bin/open -a /Applications/Utilities/Console.app $LOG
+	echo -n "Creating .ipa for ${PRODUCT_NAME}... " > $LOG
+fi
 
 /bin/rm "/tmp/${PRODUCT_NAME}.ipa"
 /usr/bin/xcrun -sdk iphoneos PackageApplication -v "${APP}" -o "/tmp/${PRODUCT_NAME}.ipa" --sign "${SIGNING_IDENTITY}" --embed "${PROVISIONING_PROFILE}"
 
-#echo "done." >> $LOG
-echo "Created .ipa for ${PRODUCT_NAME}" | ${GROWL}
-
-#echo -n "Zipping .dSYM for ${PRODUCT_NAME}..." >> $LOG
-echo "Zipping .dSYM for ${PRODUCT_NAME}" | ${GROWL}
+if [ -n "$LOG" ]; then
+	echo "done." >> $LOG
+	echo -n "Zipping .dSYM for ${PRODUCT_NAME}..." >> $LOG
+fi
 
 /bin/rm "/tmp/${PRODUCT_NAME}.dSYM.zip"
 /usr/bin/zip -r "/tmp/${PRODUCT_NAME}.dSYM.zip" "${DSYM}"
 
-#echo "done." >> $LOG
-echo "Created .dSYM for ${PRODUCT_NAME}" | ${GROWL}
-
-#echo -n "Uploading to TestFlight... " >> $LOG
-echo "Uploading to TestFlight" | ${GROWL}
+if [ -n "$LOG" ]; then
+	echo "done." >> $LOG
+	echo -n "Uploading to TestFlight... " >> $LOG
+fi
 
 /usr/bin/curl "http://testflightapp.com/api/builds.json" \
   -F file=@"/tmp/${PRODUCT_NAME}.ipa" \
@@ -46,5 +63,8 @@ echo "Uploading to TestFlight" | ${GROWL}
   -F team_token="${TEAM_TOKEN}" \
   -F notes="Build uploaded automatically from Xcode."
 
-#echo "done." >> $LOG
-echo "Uploaded to TestFlight" | ${GROWL} -s && /usr/bin/open "https://testflightapp.com/dashboard/builds/"
+if [ -n "$LOG" ]; then
+	echo "done." >> $LOG
+	echo "Opening browser to $DONE_URL" >> $LOG
+fi
+/usr/bin/open "$DONE_URL"
